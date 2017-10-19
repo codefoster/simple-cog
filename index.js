bd9b9a3dabc73e87d8b3c9cdf8e7503a708ee474
Jeremy Foster <jeremy.foster@live.com>
2017-10-19 11:34:54 -0700

    perform analyzeImage call

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('hello simple-cog!')
+let cognitiveServices = require('cognitive-services');
+
+let client = new cognitiveServices.computerVision({
+    apiKey: "<key>",
+    endpoint: "<endpoint>"
+});
+
+let parameters = {
+    "visualFeatures": "ImageType,Faces,Adult,Categories,Color,Tags,Description",
+    "details": "Celebrities,Landmarks"
+};
+
+let headers = {
+    'Content-type': 'application/json'
+};
+
+var body = {
+    "url": 'http://az616578.vo.msecnd.net/files/2016/10/09/636115830685164048-686058602_friends.jpg'
+};
+
+client.analyzeImage({
+    parameters,
+    headers,
+    body
+}).then((response) => {
+    console.log(response);
+});
